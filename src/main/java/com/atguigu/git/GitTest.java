e641e486f60b4e6e3e4dc5467ce0e86bde0af472
sgd <smile_boy@foxmail.com>
2021-07-28 17:07:26 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author sgd
+ * @date 2021/7/28下午5:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
