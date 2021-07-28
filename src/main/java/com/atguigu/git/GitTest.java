16e3dc56f020929b297998f12d83744d751927e2
sgd <smile_boy@foxmail.com>
2021-07-28 17:32:04 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master git!");
         System.out.println("hot-fix git!");
+        System.out.println("push git!");
     }
 }
